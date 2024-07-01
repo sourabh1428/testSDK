76a7ffa58232f3bcc2914c5278f5c4b8e3de5815
Sourabh <sppathak1428@gmail.com>
2024-07-02 00:41:49 +0530

    as

--- SDK/index.js
+++ SDK/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-const Bottleneck = require("bottleneck");
 
+require('dotenv').config();
 
 const RequestQueue = require("./requestHandler");
 
--- routes/campaign.js
+++ routes/campaign.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 // MongoDB connection
-const uri = "mongodb+srv://sppathak1428:1vmAkV1LIypO4bVQ@cluster0.hldmans.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+require('dotenv').config()
+
+
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
