26d687710b262394514502d6605318c2b8eb08bd
Sourabh <sppathak1428@gmail.com>
2024-09-15 19:26:18 +0530

    Das

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.listen(port, () => {
 // async function hue(){
 //   let ans=[];
 //   ans=await getAllCampaigns();
-
+//12
 //       for(let i=0;i<ans.length;i++){
 //           let x=UIS(ans[i].segment_id);
 //           console.log("segment refreshed it's segment id: "+ans[i].segment_id);
